2d25add6890a3312f1d3d2e8881d65ff741e4be9
Zotho <svjatoslavalekseef2@gmail.com>
2021-04-24 08:36:09 +0500

    Fix raw_bench

--- examples/raw_bench.rs
+++ examples/raw_bench.rs
@@ -2,8 +2,8 @@ use sandpiles_parallel::Field;
 
 fn main() {
     let mut field: Field<u32> = Field::new(1000, 1000);
-    field.data[500_000] = 100;
-    for _ in 0..1000 {
-        field.update_iter_branchless();
+    field.data[500_000] = 10000;
+    for _ in 0..10000 {
+        field.update_parallel();
     }
 }
